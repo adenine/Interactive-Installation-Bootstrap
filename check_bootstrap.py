851b4d24a481a8b12bf9405d700618460d6d306d
Joshua Walton <adenine@gmail.com>
2012-05-30 22:25:20 -0400

    Modified command line tests

--- check_bootstrap.py
+++ check_bootstrap.py
@@ -1,10 +1,10 @@
 import envoy
 
-bootstrap_status = {'softwareupdate':'default', 'hostname':'default'}
-r = envoy.run('sudo softwareupdate --schedule', timeout=2)
+bootstrap_status = {'softwareupdate':'default' 'hostname':'default'}
+r = envoy.run('sudo softwareupdate --schedule' timeout=2)
 bootstrap_status['softwareupdate'] = r.std_out
 
-r = envoy.run('sudo scutil --get HostName', timeout=2)
+r = envoy.run('sudo scutil --get HostName' timeout=2)
 bootstrap_status['hostname'] = r.std_out
 #$ scutil --get LocalHostName
 #$ scutil --get ComputerName
--- command_line_tests.sh
+++ command_line_tests.sh
@@ -2,22 +2,52 @@
 # This script tries to look at whether an osx 10.7 machine is ready for use
 # as a machine in an interactive installation.
 
+# check to see if software update it automatically running
+echo "SOFTWARE UPDATE ------------------------------------"
 softwareupdate --schedule
 
+# look at all the various places that osx saves names for the computer
+echo "HOSTNAME -------------------------------------------"
 scutil --get HostName 
 scutil --get LocalHostName
 scutil --get ComputerName
 
+# Check the sleep setting and if it is set to wake
+echo "SLEEP SETTINGS -------------------------------------"
 systemsetup -getsleep 
 systemsetup -getwakeonnetworkaccess
-
 systemsetup -getallowpowerbuttontosleepcomputer 
 systemsetup -getrestartpowerfailure 
 
-defaults read /Library/Preferences/com.apple.loginwindow
+# Check power up and down schedule
+echo "POWER SETTINGS -------------------------------------"
+pmset -g sched
 
-osascript -e 'tell application "System Events" to get the name of every login item'
+# Look for users (omit all of the default users)
+echo "USERS ----------------------------------------------"
+dscl . list /Users | grep -v "^_"
 
-# defaults write com.google.Keystone.Agent checkInterval 0
+echo "CRASH REPORTING ------------------------------------"
+defaults read com.apple.CrashReporter DialogType
 
-# defaults write com.apple.CrashReporter DialogType none
+echo "CHECK EXPOSE BEING DISABLED ------------------------"
+defaults read com.apple.dock mcx-expose-disabled
+
+echo "CHECK DOCK AUTOHIDE --------------------------------"
+defaults read com.apple.dock autohide #from here we can check what apps are in the dock
+
+echo "CHECK SCREENSAVER IDLE TIME (Should be 0) ----------"
+defaults -currentHost read com.apple.screensaver idleTime
+
+echo "REMOTE LOGIN ----------------------------------------"
+systemsetup -getremotelogin 
+
+echo "APPLE REMOTE EVENTS ---------------------------------"
+systemsetup -getremoteappleevents 
+
+#echo "CHECK DASHBOARD BEING DISABLED ---------------------"
+# defaults read com.apple.dashboard 
+
+# defaults read /Library/Preferences/com.apple.loginwindow
+
+# osascript -e 'tell application "System Events" to get the name of every login item'
